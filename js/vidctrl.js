c5224ec7608811f427252a198b2b066609c74f70
TJ <uko@livemail.tw>
2021-07-30 22:28:27 +0800

    debug

--- js/vidctrl.js
+++ js/vidctrl.js
@@ -165,6 +165,7 @@ function brightness(val) {
     b += val;
 
     adjFilter();
+    $('#bright').val(s);
     $(".toast-body")[0].innerText = "亮度: " + b + "%";
     $(".toast").toast("show");
 }
@@ -173,6 +174,7 @@ function saturate(val) {
     s += val;
 
     adjFilter();
+    $('#saturation').val(s);
     $(".toast-body")[0].innerText = "飽和度: " + s + "%";
     $(".toast").toast("show");
 }
@@ -181,6 +183,7 @@ function contrast(val) {
     c += val;
 
     adjFilter();
+    $('#contrast').val(s);
     $(".toast-body")[0].innerText = "對比度: " + c + "%";
     $(".toast").toast("show");
 }
