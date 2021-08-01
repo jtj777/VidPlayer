07b8c40392051c8e16a9a062814a8683fd786837
TJ <uko@livemail.tw>
2021-08-01 14:53:58 +0800

    resize debug

--- js/vidctrl.js
+++ js/vidctrl.js
@@ -208,7 +208,7 @@ function adjFilter() {
 window.onresize = function () {
     if (!player) player = document.getElementById('player');
     setFitSize();
-    showToast("影片尺寸:" + $('#player').width() + "x" + $('#player').height());
+    showToast("影片尺寸:" + $('#player').videoWidth() + "x" + $('#player').videoHeight());
 };
 
 function enableABLoop(a, b) {
