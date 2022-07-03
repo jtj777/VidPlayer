4c3ecc69ca878db66eb9fd6af5155a6bb41b3967
TJ <uko@livemail.tw>
2022-07-03 15:45:57 +0800

    Update embed.js

--- js/embed.js
+++ js/embed.js
@@ -155,7 +155,7 @@ function main() {
     }
 
     //移動
-    player.onmousedown(function (e) {
+    player.onmousedown = (e) => {
         //設定移動後的預設位置
         var endx = 0;
         var endy = 0;
@@ -174,7 +174,7 @@ function main() {
             player.css("left", endx).css("top", endy);
         });
     })
-    player.mouseup(function () {
+    player.mouseup = () => {
         //滑鼠彈起時給div取消事件
         player.unbind("onmousemove")
     })
